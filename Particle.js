49e592da279d1ccdb920267e199c1201212618c0
Nikitad789 <84267881+Nikitad789@users.noreply.github.com>
2021-06-28 14:11:03 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,33 @@
+class particle{
+    constructor(x,y){
+      var  options={
+
+        }
+
+    this.x=x
+    this.y=y
+    this.r=10
+
+       this.body= Bodies.circle(this.x,this.y,this.r,options)
+       this.color=color(random(0,255),random(0,255),random(0,255))
+       World.add(world,this.body)
+    }
+    display(){
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        noStroke();
+        fill(this.color);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+          }
+}
+
+
+
